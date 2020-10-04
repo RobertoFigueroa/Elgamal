4562ac80367b55ff5210f0fad064f29e3974e33e
Gustavo Méndez <gus.mendez.99@gmail.com>
2020-10-03 22:59:40 -0600

    Update elgamal_with_lib.py

--- elgamal_with_lib.py
+++ elgamal_with_lib.py
@@ -1,1 +1,11 @@
+from elgamal.elgamal import Elgamal
 
+# Declare Alice message, parsed to bytes
+m = b'Hi Bob! How are you?'
+print(m)
+
+# Gets the public and private key, using 256 bits.
+public_key, private_key = Elgamal.newkeys(256)
+
+print("Public: {}".format(public_key))
+print("Private: {}".format(private_key))
