385d020015988b9dc0def780c64e313e4f5113e9
Luis Urbina <luis212urbina@gmail.com>
2020-10-03 23:04:22 -0600

    added encryption part and decrtyption.

--- elgamal_with_lib.py
+++ elgamal_with_lib.py
@@ -9,3 +9,13 @@ public_key, private_key = Elgamal.newkeys(256)
 
 print("Public: {}".format(public_key))
 print("Private: {}".format(private_key))
+
+# Alice encrypts message with public key
+ct = Elgamal.encrypt(m, public_key)
+print("Alice encrypted message is ", ct)
+
+# Alice send the message, and the private key is shared to Bob to decrypt the message
+decrypted_message = Elgamal.decrypt(ct, private_key)
+
+# Bob reads decrypted message
+print("Bob reads: {}".format(decrypted_message.decode("utf-8") ))
