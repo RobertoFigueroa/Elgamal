8f6626d7ee46dbd0e755db44f3bf4eecc639e73f
michelebenvenuto <35434145+michelebenveuto@users.noreply.github.com>
2020-10-03 23:12:53 -0600

    Update elgamal_no_lib.py

--- elgamal_no_lib.py
+++ elgamal_no_lib.py
@@ -1,1 +1,14 @@
 
+print("-"*20) 
+print("Bob's side")   
+print("-"*20) 
+#generate random prime number
+q = random.randint(pow(10, 20), pow(10, 50)) 
+#generate g
+g = random.randint(2, q)
+#generate a that has to be coprime with q (this is my secrete key)
+a = gen_key(q)
+#generate h
+h = power(g, a, q)
+#We send to Alice [q, g, h] which is my public key
+public_key = [q, g, h]
